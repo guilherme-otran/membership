f4b6a5c9cb9ea319321126c6064892cab59ef0f9
Guilherme O <guilherme_otran@hotmail.com>
2018-02-10 17:39:49 -0200

    Mailer settings

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,11 @@
+if ENV["MAIL_ADDRESS"].present?
+  ActionMailer::Base.smtp_settings = {
+    :address        => ENV["MAIL_ADDRESS"],
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['MAIL_USERNAME'],
+    :password       => ENV['MAIL_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+end
